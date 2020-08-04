9ec13dce63152ceb5464a0cb25e56a3b941f1f59
anh tong <anhth@unist.ac.kr>
2020-08-04 20:51:44 +0900

    update

--- src/structural_sgp.py
+++ src/structural_sgp.py
@@ -1,9 +1,10 @@
+import numpy as np
 import torch
 from torch.nn import ModuleList
 from gpytorch.module import Module
 
 import gpytorch
-from gpytorch.constraints import Interval
+from gpytorch.constraints import Interval, Positive
 from gpytorch.models import ApproximateGP
 from gpytorch.distributions import MultivariateNormal
 from gpytorch.variational import CholeskyVariationalDistribution, VariationalStrategy
@@ -66,30 +67,134 @@ class SpikeAndSlabSelector(BaseSparseSelector):
                                 parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
         self.register_parameter(name="log_w_zero",
                                 parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
+        self.register_parameter(name="raw_sparsity",
+                                parameter=torch.nn.Parameter(torch.zeros(1)))
+        sparsity_constraint = Interval(0., 1.)
+        self.register_constraint("raw_sparsity", sparsity_constraint)
+        self.register_parameter(name="raw_scale",
+                                parameter=torch.nn.Parameter(torch.zeros(1)))
 
     @property
     def prob(self):
         return self.raw_prob_constraint.transform(self.raw_prob)
 
-    def kl_divergence(self):
-        log_prior = torch.sum(self.prob * tor)
-        entropy = 0.
-        return entropy + log_prior
+    @property
+    def sparsity_prior(self):
+        return self.raw_sparsity_constraint.transform(self.raw_sparsity)
 
-    def gumbel_sample(self):
+    @property
+    def scale_prior(self):
+        # return torch.tensor([1.])
+        return torch.exp(self.raw_scale) + 1e-20
+
+    def kl_divergence(self):
+
+        eps = 1e-20 # make computation stable for log
+        log_prior = - 0.5 * self.dim * (torch.log(2 * np.pi * self.scale_prior)) - \
+                    0.5 * (torch.exp(self.log_w_var) + torch.square(self.w_mean)).sum() / self.scale_prior + \
+                    (self.prob * torch.log(self.sparsity_prior + eps)).sum() + \
+                    ((1. - self.prob) * torch.log(1. - self.sparsity_prior + eps)).sum()
+        entropy = - self.prob * torch.log(self.prob) - \
+                  (1. - self.prob) * torch.log(1. - self.prob) + \
+                  0.5 * (1 - self.prob) * (torch.log(2. * np.pi * torch.exp(torch.tensor([1.]))) + self.log_w_zero)\
+                  + 0.5 * self.prob * (torch.log(2. * np.pi * torch.exp(torch.tensor([1.]))) + self.log_w_var)
+        entropy = entropy.sum()
+
+        return -entropy - log_prior
+
+    def gumbel_sample(self, eps=1e-20):
         bar_prob = 1. - self.prob
         stacked = torch.cat([bar_prob, self.prob], dim=1)
         uniform = torch.rand(self.dim, 2)
-        z = - torch.log(-torch.log(uniform)) + torch.log(stacked)
+        z = - torch.log(-torch.log(uniform + eps) + eps) + torch.log(stacked)
         applied_softmax = torch.softmax(z, dim=1) / self.gumbel_temp
-        return applied_softmax[:,1]
+        return applied_softmax[:, 1]
 
     def __call__(self):
-
-        g = self.gumbel_sample()[:,None]
+        g = self.gumbel_sample()[:, None]
         mean = g * self.w_mean
-        var = g * torch.exp(self.log_w_var) + (1. - g) * torch.exp(self.log_w_zero)
+        # var = g * torch.exp(self.log_w_var) + (1. - g) * torch.exp(self.log_w_zero)
+        var = g * torch.exp(self.log_w_var) + (1. - g) * self.scale_prior
         return mean + var * torch.randn(self.dim, 1)
+
+class SpikeAndSlabSelectorV2(BaseSparseSelector):
+
+    def __init__(self, dim, gumbel_temp=0.1):
+        super().__init__(dim)
+        self.gumbel_temp = gumbel_temp
+        self.register_parameter(name="w_mu", parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
+        self.register_parameter(name="raw_w_s2", parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
+        w_s2_constraint = Positive()
+        self.register_constraint("raw_w_s2", w_s2_constraint)
+        self.register_parameter(name="raw_s_pi", parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
+        s_pi_constraint = Interval(0., 1.)
+        self.register_constraint("raw_s_pi", s_pi_constraint)
+        self.register_parameter(name="raw_pi_w", parameter=torch.nn.Parameter(torch.zeros(self.dim, 1)))
+        pi_w_constraint = Interval(0., 1.)
+        self.register_constraint("raw_pi_w", pi_w_constraint)
+        self.register_parameter(name="raw_s2_w", parameter=torch.nn.Parameter(torch.zeros(1)))
+        s2_w_constraint = Positive()
+        self.register_constraint("raw_s2_w", s2_w_constraint)
+
+        # Only for linear model
+        self.register_parameter(name="raw_s2", parameter=torch.nn.Parameter(torch.zeros(1)))
+        s2_constraint = Positive()
+        self.register_constraint("raw_s2", s2_constraint)
+
+
+    @property
+    def w_s2(self):
+        return self.raw_w_s2_constraint.transform(self.raw_w_s2)
+
+    @property
+    def s_pi(self):
+        return self.raw_s_pi_constraint.transform(self.raw_s_pi)
+
+    @property
+    def pi_w(self):
+        return self.raw_pi_w_constraint.transform(self.raw_pi_w)
+
+    @property
+    def s2_w(self):
+        return self.raw_s2_w_constraint.transform(self.raw_s2_w)
+
+    @property
+    def s2(self):
+        return self.raw_s2_constraint.transform(self.raw_s2)
+
+    def gumbel_sample(self, eps=1e-20):
+        bar_s_pi = 1. - self.s_pi
+        stacked = torch.cat([bar_s_pi, self.s_pi], dim=1)
+        uniform = torch.rand(self.dim, 2)
+        z = - torch.log(-torch.log(uniform + eps) + eps) + torch.log(stacked)
+        applied_softmax = torch.softmax(z, dim=1) / self.gumbel_temp
+        return applied_softmax[:, 1]
+
+    def __call__(self):
+        g = self.gumbel_sample()[:, None]
+        mean = g * self.w_mu
+        var = g * self.w_s2 + (1-g) * self.s2_w
+        return g * (mean + torch.sqrt(var) * torch.randn(self.dim, 1))
+
+
+    def kl_divergence(self):
+
+        esp = 1e-20
+
+        g = self.gumbel_sample()[:, None]
+        mean = g * self.w_mu
+        var = g * self.w_s2 + (1-g) * self.s2_w
+        w =  mean + torch.sqrt(var) * torch.randn(self.dim, 1)
+        mean = mean.detach()
+        var = var.detach()
+        entropy = 0.5 * torch.sum(torch.log(2. * np.pi * var)+ 0.5 * torch.square(w-mean) / var) \
+                  - torch.sum(g * torch.log(self.s_pi + esp) + (1.-g) * torch.log(1. - self.s_pi + esp))
+
+        log_prior = - 0.5 * self.dim * torch.log(2. * np.pi * self.s2_w) \
+                    - 0.5 * torch.sum(torch.square(w)) / self.s2_w + \
+                    torch.sum(g * torch.log(self.pi_w + esp) + (1. - g) * torch.log(1. - self.pi_w + esp))
+
+        return - log_prior - entropy
 
 
 class StructuralSparseGP(ApproximateGP):
--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from gpytorch.means import ZeroMean
 from gpytorch.kernels import RBFKernel, PeriodicKernel
-from src.structural_sgp import VariationalGP, StructuralSparseGP, TrivialSelector, SpikeAndSlabSelector
+from src.structural_sgp import VariationalGP, StructuralSparseGP, TrivialSelector, SpikeAndSlabSelector, SpikeAndSlabSelectorV2
 from src.mean_field_hs import MeanFieldHorseshoe, VariatioalHorseshoe
 from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.mlls import VariationalELBO
@@ -36,7 +36,7 @@ def test_trivial_model():
     elbo = VariationalELBO(likelihood, model, num_data=100)
 
     output = model(train_x)
-    optimizer = torch.optim.Adam(list(model.parameters()) + list(likelihood.parameters()), lr=0.01)
+    optimizer = torch.optim.Adam(list(model.parameters()) + list(likelihood.parameters()), lr=0.1)
 
 
     for i in range(500):
@@ -63,8 +63,132 @@ def test_object_spike_and_slab():
     ss = SpikeAndSlabSelector(5)
     sample = ss()
     print(sample)
+    print(ss.kl_divergence())  ## KL divergence equal to 0 makes sense because the variational dist. and prior dist. is the same
+
+def test_object_spike_and_slab_v2():
+    ss = SpikeAndSlabSelectorV2(5)
+    sample = ss()
+    print(sample)
     print(ss.kl_divergence())
 
 
+def test_linear_regression_spike_and_slab():
+
+    ### CREATE DATA
+    N = 100
+    sparsity = 0.05
+    M = 200
+    beta = np.zeros(M+1)
+    b1 = np.random.binomial(n=1, p=sparsity, size=M)
+    b2 = np.random.binomial(n=1, p=0.5, size=M)
+    for m in range(M):
+        if b1[m]:
+            if b2[m]:
+                beta[m] = 10 + np.random.randn()
+            else:
+                beta[m] = -10 + np.random.randn()
+        else:
+            beta[m] = 0.25 * np.random.randn()
+
+    beta[M] = 0.
+    X_train = np.random.randn(N, M+1)
+    X_train[:,M] = 1
+    y_train = np.matmul(X_train, beta) + np.random.randn(N)
+    X_train, y_train = torch.tensor(X_train, dtype=torch.float32), torch.tensor(y_train, dtype=torch.float32)
+    spike_and_slab = SpikeAndSlabSelector(dim=M+1)
+
+    def compute_loss():
+        s2 = torch.tensor(1e-2)
+        w = spike_and_slab()
+        def log_likelihood(w):
+            y_mean = X_train @ w
+            ll = - 0.5 * N * torch.log(2. * np.pi * s2) - 0.5 * torch.square(y_train - y_mean).sum() / s2
+            return ll
+
+        elbo = log_likelihood(w) - spike_and_slab.kl_divergence()
+        return -elbo
+
+    optimizer = torch.optim.Adam(spike_and_slab.parameters(), lr=0.1)
+
+    for i in range(500):
+        optimizer.zero_grad()
+        loss = compute_loss()
+        loss.backward()
+        optimizer.step()
+        print("Iter: {} \t Loss: {}".format(i, loss.item()))
+
+def test_linear_regression_spike_and_slab_v2():
+
+    ### CREATE DATA
+    N = 100
+    sparsity = 0.05
+    M = 200
+    beta = np.zeros(M+1)
+    b1 = np.random.binomial(n=1, p=sparsity, size=M)
+    b2 = np.random.binomial(n=1, p=0.5, size=M)
+    for m in range(M):
+        if b1[m]:
+            if b2[m]:
+                beta[m] = 10 + np.random.randn()
+            else:
+                beta[m] = -10 + np.random.randn()
+        else:
+            beta[m] = 0.25 * np.random.randn()
+
+    beta[M] = 0.
+    X_train = np.random.randn(N, M+1)
+    X_train[:,M] = 1
+    y_train = np.matmul(X_train, beta) + np.random.randn(N)
+    X_train, y_train = torch.tensor(X_train, dtype=torch.float32), torch.tensor(y_train, dtype=torch.float32)
+    spike_and_slab = SpikeAndSlabSelectorV2(dim=M+1)
+
+    def compute_loss():
+        s2 = spike_and_slab.s2
+        w = spike_and_slab()
+        def log_likelihood(w):
+            y_mean = X_train @ w
+            ll = - 0.5 * N * torch.log(2. * np.pi * s2) - 0.5 * torch.square(y_train - y_mean).sum() / s2
+            return ll
+
+        elbo = log_likelihood(w) - spike_and_slab.kl_divergence()
+        return -elbo
+
+    optimizer = torch.optim.Adam(spike_and_slab.parameters(), lr=0.1)
+
+    for i in range(1000):
+        optimizer.zero_grad()
+        loss = compute_loss()
+        loss.backward()
+        optimizer.step()
+        print("Iter: {} \t Loss: {}".format(i, loss.item()))
+
+    fig = plt.figure(figsize=(16,8))
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(np.arange(M), beta[:-1], \
+            linewidth=3, color="black", label="ground truth")
+    ax.scatter(np.arange(M), beta[:-1], \
+               s=70, marker='+', color="black")
+    w =spike_and_slab.w_mu * spike_and_slab.s_pi
+    w = w.detach().numpy()
+    ax.plot(np.arange(M), w[:-1], \
+            linewidth=3, color="red", \
+            label="linear model with spike and slab prior")
+    ax.set_xlim([0, M - 1])
+    ax.set_ylabel("Slopes", fontsize=18)
+    ax.hlines(0, 0, M - 1)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().set_visible(False)
+    ax.legend(prop={'size': 14})
+
+    fig.set_tight_layout(True)
+    fig.savefig('foo.png')
+    plt.show()
+
+
 # test_trivial_model()
-test_object_spike_and_slab()
+# test_object_spike_and_slab()
+# test_object_spike_and_slab_v2()
+test_linear_regression_spike_and_slab_v2()
