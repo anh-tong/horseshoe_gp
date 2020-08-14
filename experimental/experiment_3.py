1b44f3a79f788933dd7e32c860fa8d619b5bf054
anh tong <anhth@unist.ac.kr>
2020-08-14 20:40:10 +0900

    [incomplete] create experiment file

---
+++ experimental/experiment_3.py
@@ -0,0 +1,28 @@
+"""
+Regression experiment
+
+Data set:
+    - UCI
+    - ABCD data set
+    - Large scale data set
+"""
+
+import torch
+from src.structural_sgp import StructuralSparseGP
+from src.sparse_selector import HorseshoeSelector, SpikeAndSlabSelector
+
+def load_data():
+    pass
+
+def create_model():
+    pass
+
+def train():
+    pass
+
+def test():
+    pass
+
+def run():
+
+    x_train, y_train, x_test, y_test = load_data()
