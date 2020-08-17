ebf8d173c47c7658c081fa3c8e6d8f19c6e57860
wngud0811 <41232255+wngud0811@users.noreply.github.com>
2020-08-17 19:35:52 +0900

    not working

--- experimental_jh/bayesian_optimization/.ipynb_checkpoints/main-checkpoint.py
+++ experimental_jh/bayesian_optimization/.ipynb_checkpoints/main-checkpoint.py
@@ -37,8 +37,13 @@ args = parser.parse_args()
 
 import math
 import torch
-is_cuda = torch.cuda.is_available()
+#if torch.cuda.is_available():
+#    device = torch.device('cuda')
+#else:
+#    device = torch.device('cpu')
+device = torch.device('cpu')
 torch.manual_seed(2020)
+#torch.autograd.set_detect_anomaly(True)
 
 import gpytorch
 from gpytorch.means import ZeroMean
@@ -50,31 +55,46 @@ from src.structural_sgp import VariationalGP, StructuralSparseGP
 exec("from src.sparse_selector import " + args.selector)
 
 exec("from botorch.acquisition.analytic import " + args.acq_fun)
+from botorch.generation.gen import gen_candidates_torch
 from botorch.generation.gen import gen_candidates_scipy
+#Standard setting is to use L-BFGS-D for optimizing acqusition function
+#But due to numerical issue in Botorch(https://github.com/pytorch/botorch/issues/179), used Adam in torch 
+from botorch.posteriors.gpytorch import GPyTorchPosterior
+from botorch.models.utils import gpt_posterior_settings
+from botorch.models.gpytorch import GPyTorchModel
 
 from utils import branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenbrock
+
+from typing import Any, Union
+#def posterior(self, X: torch.Tensor, observation_noise: Union[bool, torch.Tensor] = False, **kwargs: Any) -> GPyTorchPosterior:
+    #self.eval()
+    #with gpt_posterior_settings():
+    
+StructuralSparseGP.posterior = GPyTorchModel.posterior
+    
+#StructuralSparseGP.posterior = posterior
 
 for opt in [branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenbrock]:
     bench_fun = opt()
     
     n_inducing = args.num_inducing
-    inducing_points = torch.stack([torch.linspace(0, 1, n_inducing)] * bench_fun.dim, dim = 1)
+    inducing_points = torch.stack([torch.linspace(0, 1, n_inducing)] * bench_fun.dim, dim = 1).to(device)
 
     # set up kernels
     n_kernels = args.n_kernels
-    means = [ZeroMean()] * n_kernels
-    kernels = [RBFKernel()] * n_kernels
+    means = [ZeroMean().to(device)] * n_kernels
+    kernels = [RBFKernel().to(device)] * n_kernels
     
     # GP for each kernel
     gps = []
     for mean, kernel in zip(means, kernels):
-        gp = VariationalGP(mean, kernel, inducing_points)
+        gp = VariationalGP(mean, kernel, inducing_points).to(device)
         gps.append(gp)
 
     exec("selector = " + args.selector + "(dim=n_kernels, A=1., B=1.)")
-    model = StructuralSparseGP(gps, selector)
+    model = StructuralSparseGP(gps, selector).to(device)
 
-    likelihood = GaussianLikelihood()
+    likelihood = GaussianLikelihood().to(device)
     elbo = PredictiveLogLikelihood(likelihood, model, num_data=args.num_raw_samples)
     
     if args.acq_fun == "UpperConfidenceBound":
@@ -83,54 +103,37 @@ for opt in [branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenb
         exec("acq_fun = " + args.acq_fun + "(model, best_f=0.2)")
         
     #Initial Points given
-    x = torch.empty(args.num_raw_samples, bench_fun.dim) * (bench_fun.bound[1] - bench_fun.bound[0]).view(1, -1) + bench_fun.bound[0].view(1, -1)
-    y = bench_fun.value(x)
+    x = torch.empty(args.num_raw_samples, bench_fun.dim).to(device)
+    y = bench_fun(x)
     
-    #Load modules to GPU
-    if is_cuda:
-        model = model.cuda()
-        likelihood = likelihood.cuda()
-        elbo = elbo.cuda()
-        
-        inducing_points = inducing_points.cuda()
-        x = x.cuda()
-        y = y.cuda()
-    
-    optimizer = torch.optim.Adam(list(model.parameters()) + list(likelihood.parameters()), lr=args.learning_rate)
+    optimizer = torch.optim.Adam(
+        list(model.parameters()) + list(likelihood.parameters()),
+        lr=args.learning_rate)
     
     #Initiali Training
     print("---Initial training---")
     for ind in range(args.num_step):
         optimizer.zero_grad()
         selector.update_tau_lambda()
-        output = model(x)
-        loss = - elbo(output, y)
+        loss = - elbo(model(x), y)
         loss.backward(retain_graph=True)
         optimizer.step()
         print("Iter: {} \t Loss: {:.2f}".format(ind, loss.item()))
         
-    batch_candidates, batch_acq_values = gen_candidates_scipy(
-        initial_conditions=x.view(-1, 1, bench_fun.dim),
-        acquisition_function=acq_fun,
-        lower_bounds = bench_fun.bound[0],
-        upper_bounds = bench_fun.bound[1])
-    
-    x = torch.cat([x, batch_candidates], dim = 0)
-    y = torch.cat([y, bench_fun(batch_candidates)], dim = 0)
-        
     #Bayesian Optimization iteration
     for tries in range(args.num_trial):
-        print("try: %d" %tries)
-        pass
+        print("try: %d" %(tries + 1))
+        batch_candidates, batch_acq_values = gen_candidates_torch(
+            initial_conditions=x.view(-1, 1, bench_fun.dim),
+            acquisition_function=acq_fun,
+            lower_bounds = bench_fun.lower_bound,
+            upper_bounds = bench_fun.upper_bound)
+        batch_candidates = batch_candidates.view(x.shape)
+
+        x = torch.cat([x, batch_candidates], dim = 0)
+        y = torch.cat([y, bench_fun(batch_candidates)], dim = 0)
         
-        elbo = PredictiveLogLikelihood(likelihood, model, num_data=args.num_raw_samples + tries)
-        if is_cuda:
-            elbo = elbo.cuda()
-        else:
-            x = torch.cat([x], dim = 0)
-            y = torch.cat([y], dim = 0)
-                
-        optimizer = torch.optim.Adam(list(model.parameters()) + list(likelihood.parameters()), lr=0.01)
+        elbo.num_data += 1
         
         for ind in range(args.num_step):
             optimizer.zero_grad()
@@ -138,8 +141,11 @@ for opt in [branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenb
             loss = - elbo(model(x), y)            
             loss.backward(retain_graph=True)
             optimizer.step()
+            print("Iter: {} \t Loss: {:.2f}".format(ind, loss.item()))
     
-    print(bench_fun.sol)
+    print("result:")
+    print(model(x[-1]))
+    print(y[-1])
     
     
         
--- experimental_jh/bayesian_optimization/utils.py
+++ experimental_jh/bayesian_optimization/utils.py
@@ -4,10 +4,12 @@ import torch
 ###Benchmark Black-Box Funtions
 #https://arxiv.org/pdf/1308.4008.pdf
 
-class acq_fun:
-    def __init__(self, bound, sols, sol_val):
+class acq_fun(torch.nn.Module):
+    def __init__(self, lower_bound, upper_bound, sols, sol_val):
+        super(acq_fun, self).__init__()
         self.dim = None
-        self.bound = torch.Tensor(bound)
+        self.lower_bound = torch.nn.Parameter(torch.Tensor(lower_bound))
+        self.upper_bound = torch.nn.Parameter(torch.Tensor(upper_bound))
         self.sols = sols
         self.sol_val = sol_val
         
@@ -20,40 +22,43 @@ class acq_fun:
                 return False
         return True
 
-    def value(self, x):
+    def forward(self, x):
         raise NotImplementedError()
 
 class branin_rcos(acq_fun):
     def __init__(self):
         super().__init__(
-                         [[-5, 0], [10, 15]],
-                         [(-math.pi, 12.275), (math.pi, 2.275), (3*math.pi, 2.475)],
-                         0.39788735772973816)
+            [-5, 0],
+            [10, 15],
+            [(-math.pi, 12.275), (math.pi, 2.275), (3*math.pi, 2.475)],
+            0.39788735772973816)
         self.dim = 2
         
-    def value(self, x):
-        x1 = x[:, 0]
-        x2 = x[:, 1]
+    def forward(self, x):
+        x1 = x[:, 0] * (self.upper_bound[0] - self.lower_bound[0]) + self.lower_bound[0]
+        x2 = x[:, 1] * (self.upper_bound[1] - self.lower_bound[1]) + self.lower_bound[1]
         return (x2 - 5.1/(4*(math.pi**2))*(x1.pow(2)) + 5/math.pi*x1 - 6).pow(2) + 10*(1-1/(8*math.pi))*torch.cos(x1)+10
 
 class six_hump_camel_back(acq_fun):
     def __init__(self):
         super().__init__(
-                        [[-5] * 2, [5] * 2],
-                        [(-0.0898, 0.7126), (0.0898, -0.7126)],
-                        -1.0316284229280819)
+            [-5] * 2,
+            [5] * 2,
+            [(-0.0898, 0.7126), (0.0898, -0.7126)],
+            -1.0316284229280819)
         self.dim = 2
         
-    def value(self, x):
-        x1, x2 = x
+    def forward(self, x):
+        x1, x2 = x * (self.upper_bound - self.lower_bound) + self.lower_bound
         return (4-2.1*(x1.pow(2))+(x1.pow(4))/3)*(x1.pow(2))+x1*x2+(4*(x2.pow(2))-4)*(x2.pow(2))
 
 class hartman_6(acq_fun):
     def __init__(self):
         super().__init__(
-                         [[0]*6, [1]*6],
-                         [(0.201690, 0.150011, 0.476874, 0.275332, 0.311652, 0.657301)],
-                         -3.306248188018799)
+            [0]*6,
+            [1]*6,
+            [(0.201690, 0.150011, 0.476874, 0.275332, 0.311652, 0.657301)],
+            -3.306248188018799)
         self.dim = 6
         self.P = torch.Tensor([
             [0.1312, 0.1696, 0.5569, 0.0124, 0.8283, 0.5586],
@@ -69,32 +74,36 @@ class hartman_6(acq_fun):
         ])
         self.c = torch.Tensor([1, 1.2, 3, 3.2])
         
-    def value(self, x): 
+    def forward(self, x):
+        x = x * (self.upper_bound - self.lower_bound) + self.lower_bound
         return float(-(self.c*(-self.A*((torch.Tensor(x).repeat(4).view(4, -1) - self.P).pow(2))).sum(1).exp()).sum())
 
 class goldstein_price(acq_fun):
     def __init__(self):
         super().__init__(
-                         [[-2]*2, [2]*2],
-                         [(0, -1)],
-                         3)
+            [-2]*2,
+            [2]*2,
+            [(0, -1)],
+            3)
         self.dim = 2
         
-    def value(self, x):
-        x1, x2 = x
+    def forward(self, x):
+        x1, x2 = x * (self.upper_bound - self.lower_bound) + self.lower_bound
         return (1 + (x1 + x2 + 1).pow(2) * (19 - 14 *x1 + 3*(x1.pow(2)) - 14*x2 + 6*x1*x2 + 3*(x2.pow(2))))*(30 + (2*x1 - 3*x2).pow(2) * (18 - 32*x1 + 12* (x1.pow(2)) + 48 * x2 - 36*x1*x2 + 27*(x2.pow(2))))
 
 class rosenbrock(acq_fun):
     def __init__(self):
         super().__init__(
-                         [[-30] * 10, [30] * 10],
-                         [(1) * 10],
-                         0)
+            [-30] * 10,
+            [30] * 10,
+            [(1) * 10],
+            0)
         self.dim = 10
                          
-    def value(self, x):
+    def forward(self, x):
         if isinstance(x, int):
             x = [x]
+        x = x * (self.upper_bound - self.lower_bound) + self.lower_bound
         res = 0.0
         for dim in range(self.dim - 1):
             res += 100 * (x[dim+1] - x[dim]**2)**2 + (x[dim] - 1)**2
