1fa2a3174cf46114d4472602dcb4a00d3d90b168
wngud0811 <41232255+wngud0811@users.noreply.github.com>
2020-08-24 10:21:55 +0900

    fixing minor bugs

--- experimental_jh/bayesian_optimization/main_gp_mattern.py
+++ experimental_jh/bayesian_optimization/main_gp_mattern.py
@@ -33,6 +33,10 @@ SpikeAndSlabSelector
 HorseshoeSelector
 ''', default = "HorseshoeSelector")
 
+parser.add_argument('--bench_fun', '-b',
+choices=["branin_rcos", "six_hump_camel_back", "hartman_6", "goldstein_price", "rosenbrock"],
+default="branin_rcos")
+
 parser.add_argument('--acq_fun', '-a',
 choices=["EI", "UCB", "POI"],
 help='''
@@ -59,12 +63,13 @@ args = parser.parse_args()
 if args.show_plot:
     import matplotlib.pyplot as plt
 
-from utils import branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenbrock
+exec("from utils import " + args.bench_fun)
+exec("bench_fun = " + args.bench_fun)
 exec("from utils import " + args.acq_fun)
 
 def acq_max(lb, ub, sur_model, y_max, acq_fun, n_warmup = 10000, iteration = 10):
     x_tries = tf.random.uniform(
-        [n_warmup, bench_fun.dim],
+        [n_warmup, obj_fun.dim],
         dtype=tf.dtypes.float64) * (ub - lb) + lb
     ys = acq_fun(x_tries, y_max)
     x_max = tf.expand_dims(x_tries[tf.squeeze(tf.argmax(ys))], 0)
@@ -81,24 +86,8 @@ def acq_max(lb, ub, sur_model, y_max, acq_fun, n_warmup = 10000, iteration = 10)
         
     for iterate in range(iteration):
         locs = tf.random.uniform(
-            [1, bench_fun.dim],
-            dtype=tf.dtypes.float64) * (ub - lb) + lb  
-        
-        """
-        res = minimize(lambda x: -acq_fun(x, y_max),
-                       tf.reshape(locs, (1, -1)),
-                       bounds=bound,
-                       method="L-BFGS-B")
-        
-        # See if success
-        if not res.success:
-            continue
-
-        if max_acq is None or -res.fun[0] >= max_acq:
-            x_max = res.x
-            max_acq = -res.fun[0]        
-        
-        """
+            [1, obj_fun.dim],
+            dtype=tf.dtypes.float64) * (ub - lb) + lb
         
         try:
             opt_result = tfp.optimizer.lbfgs_minimize(
@@ -124,75 +113,74 @@ def acq_max(lb, ub, sur_model, y_max, acq_fun, n_warmup = 10000, iteration = 10)
 
 #main
 if __name__ == "__main__":
-    bench_functions = [branin_rcos, six_hump_camel_back, hartman_6, goldstein_price, rosenbrock]
+    
     
     ###Result directory
-    save_file = "./exact_GP/"
+    save_file = "./GP_mattern/"
     
-    for opt in bench_functions:
-        df_result = pd.DataFrame(
-            0,
-            index=range(args.num_trial),
-            columns=range(args.num_init))
-        
-        bench_fun = opt()
-        
-        num_test = 0
-        while num_test < args.num_init:
-            try:
-                ###n_inducing = args.num_inducing
+    obj_fun = bench_fun()
+    df_result = pd.DataFrame(
+        0,
+        index=range(args.num_trial),
+        columns=range(args.num_init))
 
-                #Initial Points given
-                x = tf.random.uniform(
-                    (args.num_raw_samples, bench_fun.dim),
-                    dtype=tf.dtypes.float64
-                )
-                x = x * (bench_fun.upper_bound - bench_fun.lower_bound) + bench_fun.lower_bound
-                y = tf.expand_dims(bench_fun(x), 1)
+    num_test = 0
+    while num_test < args.num_init:
+        ###n_inducing = args.num_inducing
 
-                ###model
-                model = gpflow.models.GPR(
-                    data=(x, y),
-                    kernel=gpflow.kernels.Matern52(),
-                    mean_function=None)
-                model.likelihood.variance.assign(0.5)
-                model.kernel.lengthscales.assign(0.5)
+        #Initial Points given
+        x = tf.random.uniform(
+            (args.num_raw_samples, obj_fun.dim),
+            dtype=tf.dtypes.float64
+        )
+        x = x * (obj_fun.upper_bound -obj_fun.lower_bound) + obj_fun.lower_bound
+        y = tf.expand_dims(obj_fun(x), 1)
 
-                exec("acq_fun = " + args.acq_fun + "(model)")
+        ###model
+        model = gpflow.models.GPR(
+            data=(x, y),
+            kernel=gpflow.kernels.Matern52(),
+            mean_function=None)
 
-                #Initiali Training
-                optimizer = gpflow.optimizers.Scipy()
-                optimizer.minimize(
-                    model.training_loss,
-                    model.trainable_variables,
-                    options=dict(maxiter=100))
+        exec("acq_fun = " + args.acq_fun + "(model)")
 
-                #Bayesian Optimization iteration
-                for tries in range(args.num_trial):
-                    x_new, y_new = acq_max(
-                        bench_fun.lower_bound,
-                        bench_fun.upper_bound,
-                        model,
-                        tf.reduce_max(y),
-                        acq_fun)
+        #Initiali Training
+        optimizer = gpflow.optimizers.Scipy()
 
-                    x = tf.concat([x, x_new], 0)
-                    y = tf.concat([y, y_new], 0)
+        optimizer.minimize(
+            model.training_loss,
+            model.trainable_variables,
+            options=dict(maxiter=20))
 
-                    model.data = data_input_to_tensor((x, y))
-                    model.num_latent_gps += 1
+        #Bayesian Optimization iteration
+        for tries in range(args.num_trial):
+            x_new, y_new = acq_max(
+                obj_fun.lower_bound,
+                obj_fun.upper_bound,
+                model,
+                tf.reduce_max(y),
+                acq_fun)
 
-                    optimizer.minimize(
-                        model.training_loss,
-                        model.trainable_variables,
-                        options=dict(maxiter=100))
-                    
-                    #Result
-                    df_result[tries, num_test] = tf.reduce_min(y, axis=0).numpy()
-                
-                num_test += 1                
-                
-            except:
-                continue
+            x = tf.concat([x, x_new], 0)
+            y = tf.concat([y, y_new], 0)
+
+            #model.data = data_input_to_tensor((x, y))
+            #model.num_latent_gps += 1
+
+            model = gpflow.models.GPR(
+                data=(x, y),
+                kernel=gpflow.kernels.Matern52(),
+                mean_function=None)
+
+            optimizer.minimize(
+                model.training_loss,
+                model.trainable_variables,
+                options=dict(maxiter=20))
+
+            #Result
+            df_result.loc[tries, num_test] = tf.reduce_min(y, axis=0).numpy()
+
+        print(bench_fun.__name__ + "-test:%d" %(num_test + 1))
+        num_test += 1
     
-        df_result.to_csv(save_file + opt.__name__ + ".csv")
+    df_result.to_csv(save_file + bench_fun.__name__ + ".csv")
--- experimental_jh/bayesian_optimization/utils.py
+++ experimental_jh/bayesian_optimization/utils.py
@@ -5,6 +5,8 @@ import tensorflow_probability as tfp
 ###Benchmark Black-Box Funtions
 #https://arxiv.org/pdf/1308.4008.pdf
 
+pi = tf.constant(np.pi, dtype = tf.dtypes.float64)
+
 ###Acqusition Functions
 class UCB:
     def __init__(self, model):
@@ -62,55 +64,61 @@ class test_fun:
 class branin_rcos(test_fun):
     def __init__(self):
         super().__init__(
-            np.array([-5, 0], dtype = np.float64),
-            np.array([10, 15], dtype = np.float64),
-            np.array([(-np.pi, 12.275), (np.pi, 2.275), (3*np.pi, 2.475)], dtype = np.float64),
+            tf.constant([-5, 0], dtype = tf.dtypes.float64),
+            tf.constant([10, 15], dtype = tf.dtypes.float64),
+            tf.constant([(-np.pi, 12.275), (np.pi, 2.275), (3*np.pi, 2.475)], dtype = tf.dtypes.float64),
             0.39788735772973816)
         self.dim = 2
         
     def __call__(self, x):
         x1 = x[:,0]
         x2 = x[:,1]
-        return np.power((x2 - 5.1/(4*(np.power(np.pi, 2)))*np.power(x1, 2) + 5/np.pi*x1 - 6), 2) + 10*(1-1/(8*np.pi))*np.cos(x1)+10
+        return tf.pow((x2 - 5.1/(4*(tf.pow(pi, 2)))*tf.pow(x1, 2) + 5/pi*x1 - 6), 2) + 10*(1-1/(8*pi))*tf.math.cos(x1)+10
 
 class six_hump_camel_back(test_fun):
     def __init__(self):
         super().__init__(
-            np.array([-5] * 2, dtype = np.float64),
-            np.array([5] * 2, dtype = np.float64),
-            np.array([(-0.0898, 0.7126), (0.0898, -0.7126)], dtype = np.float64),
+            tf.constant([-5] * 2, dtype = tf.dtypes.float64),
+            tf.constant([5] * 2, dtype = tf.dtypes.float64),
+            tf.constant([(-0.0898, 0.7126), (0.0898, -0.7126)], dtype = tf.dtypes.float64),
             -1.0316284229280819)
         self.dim = 2
         
     def __call__(self, x):
-        x1, x2 = x
-        return (4-2.1*np.power(x1, 2)+np.power(x1, 4)/3)*np.power(x1, 2)+x1*x2+(4*np.power(x2, 2)-4)*np.power(x2, 2)
+        x1 = x[:,0]
+        x2 = x[:,1]
+        return (4-2.1*tf.pow(x1, 2)+tf.pow(x1, 4)/3)*tf.pow(x1, 2)+x1*x2+(4*tf.pow(x2, 2)-4)*tf.pow(x2, 2)
 
 class hartman_6(test_fun):
     def __init__(self):
         super().__init__(
-            np.array([0]*6, dtype = np.float64),
-            np.array([1]*6, dtype = np.float64),
-            np.array([(0.201690, 0.150011, 0.476874, 0.275332, 0.311652, 0.657301)], dtype = np.float64),
+            tf.constant([0]*6, dtype = tf.dtypes.float64),
+            tf.constant([1]*6, dtype = tf.dtypes.float64),
+            tf.constant([(0.201690, 0.150011, 0.476874, 0.275332, 0.311652, 0.657301)], dtype = tf.dtypes.float64),
             -3.306248140411212)
         self.dim = 6
-        self.P = np.array([
+        self.P = tf.constant([
             [0.1312, 0.1696, 0.5569, 0.0124, 0.8283, 0.5586],
             [0.2329, 0.4135, 0.8307, 0.3736, 0.1004, 0.9991],
             [0.2348, 0.1451, 0.3522, 0.2883, 0.3047, 0.6650],
             [0.4047, 0.8828, 0.8732, 0.5743, 0.1091, 0.0381],
-        ], dtype=np.float64)
-        self.A = np.array([
+        ], dtype=tf.dtypes.float64)
+        self.A = tf.constant([
             [10, 3, 17, 3.5, 1.7, 8],
             [0.05, 10, 17, 0.1, 8, 14],
             [3, 3.5, 1.7, 10, 17, 8],
             [17, 8, 0.05, 10, 0.1, 14],    
-        ], dtype=np.float64)
-        self.c = np.array([1, 1.2, 3, 3.2], dtype=np.float64)
+        ], dtype=tf.dtypes.float64)
+        self.c = tf.constant([1, 1.2, 3, 3.2], dtype=tf.dtypes.float64)
         
     def __call__(self, x):
-        x = x
-        return -np.sum(self.c*np.exp(np.sum(-self.A*np.power(np.reshape(np.repeat(x, 4, axis = 0), (4,6)) - self.P, 2), axis = 1)))
+        x = tf.repeat(tf.reshape(x, (-1, 1, 6)), 4, axis = 1)
+        x = -self.A*tf.pow(x - self.P, 2)
+        print(x.shape)
+        x = self.c*tf.exp(tf.reduce_sum(x, axis = 1))
+        print(x.shape)
+        x = -np.sum(x, axis = 0, keepdims = True)
+        return x
 
 class goldstein_price(test_fun):
     def __init__(self):
@@ -122,23 +130,21 @@ class goldstein_price(test_fun):
         self.dim = 2
         
     def __call__(self, x):
-        x1, x2 = x
-        return (1 + np.power(x1 + x2 + 1, 2) * (19 - 14 *x1 + 3*np.power(x1, 2) - 14*x2 + 6*x1*x2 + 3*np.power(x2, 2))*(30 + np.power(2*x1 - 3*x2, 2) * (18 - 32*x1 + 12* (np.power(x1, 2)) + 48 * x2 - 36*x1*x2 + 27*np.power(x2, 2))))
+        x1 = x[:,0]
+        x2 = x[:,1]
+        return (1 + tf.pow(x1 + x2 + 1, 2) * (19 - 14 *x1 + 3*tf.pow(x1, 2) - 14*x2 + 6*x1*x2 + 3*tf.pow(x2, 2))*(30 + tf.pow(2*x1 - 3*x2, 2) * (18 - 32*x1 + 12* (tf.pow(x1, 2)) + 48 * x2 - 36*x1*x2 + 27*tf.pow(x2, 2))))
 
 class rosenbrock(test_fun):
     def __init__(self):
         super().__init__(
-            np.array([-30] * 10, dtype=np.float64),
-            np.array([30] * 10, dtype=np.float64),
-            np.array([(1) * 10], dtype = np.float64),
+            tf.constant([-30] * 10, dtype = tf.dtypes.float64),
+            tf.constant([30] * 10, dtype = tf.dtypes.float64),
+            tf.constant([(1) * 10], dtype = tf.dtypes.float64),
             0)
         self.dim = 10
                          
     def __call__(self, x):
-        if isinstance(x, int):
-            x = [x]
-        x = x
         res = 0.0
         for dim in range(self.dim - 1):
-            res += 100 * (x[dim+1] - x[dim]**2)**2 + (x[dim] - 1)**2
+            res += 100 * (x[:, dim+1] - x[:, dim]**2)**2 + (x[:, dim] - 1)**2
         return res
